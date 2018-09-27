7a2763a4f026fd48aa3f925cf2803a7a0d1d6cd7
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2018-09-26 21:04:19 -0300

    FilterByAno

--- Painel/Homologação/dist/js/functions.js
+++ Painel/Homologação/dist/js/functions.js
@@ -1,10 +1,13 @@
-//Ano
-function filter_by_ano(ano){
+/*
+Filtro por Ano
+    Recebe o primeiro Ano da safra, exemplo: Safra de 2017 a 2018, 
+    a função deve receber o 2017 como parâmetro (inteiro) e no final, 
+    irá retornar um Objeto com as análises da safra de 2017 à 2018.
+*/
+function filterByAno(ano){
     var data = JSON.parse(localStorage.getItem("dados"));
     var cont = 0;
     var i = 0;
-    ano = ano.split('/');
-    ano = ano[0];
     var new_data = [];
 
     $.each(data, function(analises, analise){
@@ -28,7 +31,10 @@ function filter_by_ano(ano){
     return new_data;
 
 }
-
+//Retorna o primeiro ano de uma Safra, exemplo: Safra de 2017 à 2018, a função irá retornar um inteiro com valor de 2017
+function prepare_ano(valor){
+    return valor.split('/')[0];
+}
 
 // Testes
-//filter_by_ano($( "#ano-da-safra" ).val()); //Está funcionando
+//filterByAno(prepare_ano($( "#ano-da-safra" ).val())); //Está funcionando
