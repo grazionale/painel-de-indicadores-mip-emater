67026f2abd6582a3b61a0fedd9f4e189aa84f7be
Grazionale <gabrielgrazionale@hotmail.com>
2018-09-28 17:20:32 -0300

    Correção filterByRegion

--- Painel/Homologação/dist/js/functions.js
+++ Painel/Homologação/dist/js/functions.js
@@ -5,7 +5,6 @@ Filtro por Ano
     irá retornar um Objeto com as análises da safra de 2017 à 2018.
 */
 function filterByAno(ano, dados){
-
     if(dados == "" || dados == null){
         var data = JSON.parse(localStorage.getItem("dados"));
     } else {
@@ -14,9 +13,10 @@ function filterByAno(ano, dados){
     var cont = 0;
     var i = 0;
     var new_data = [];
+    //console.log(data);
 
     $.each(data, function(analises, analise){
-        
+
         ano_a = analise.surveyField.dateData.sowedDate;
         ano_a = ano_a.split('/');
         ano_a = parseInt(ano_a[2]);
@@ -32,7 +32,7 @@ function filterByAno(ano, dados){
     if(cont == 0){
         alert("Safra não encontrada no ano selecionado");
     }
-    
+    //console.log(new_data);
     return new_data;
 
 }
@@ -177,41 +177,25 @@ function filterByRegion(norte_bool, noroeste_bool, sul_bool, sudoeste_bool, oest
     }
 
     //console.log(data);
-
-    var norte = []; var n = 0;
-    var noroeste = []; var no = 0;
-    var sul = []; var s = 0;
-    var sudoeste = []; var se = 0;
-    var oeste = []; var oe = 0;
+   
     var regioes = [];
 
-    $.each(data, function(i, item) {
-        if(item.surveyField.field.city.region.macroRegion.name == "NORTE" && norte_bool == 1){
-            norte[n] = item;
-            n++;
-        } else if (item.surveyField.field.city.region.macroRegion.name == "NOROESTE" && noroeste_bool == 1){
-            noroeste[no] = item;
-            no++;
-        } else if (item.surveyField.field.city.region.macroRegion.name == "SUL" && sul_bool == 1){
-            sul[s] = item;
-            s++;
-        } else if (item.surveyField.field.city.region.macroRegion.name == "SUDOESTE" && sudoeste_bool == 1){
-            sudoeste[se] = item;
-            se++;
-        } else if (item.surveyField.field.city.region.macroRegion.name == "OESTE" && oeste_bool == 1){
-            oeste[oe] = item;
-            oe++;
+    $.each(data, function(analises, analise) {
+        if(analise.surveyField.field.city.region.macroRegion.name == "NORTE" && norte_bool == 1){
+            regioes.push(analise);
+        } else if (analise.surveyField.field.city.region.macroRegion.name == "NOROESTE" && noroeste_bool == 1){            
+            regioes.push(analise);
+        } else if (analise.surveyField.field.city.region.macroRegion.name == "SUL" && sul_bool == 1){
+            regioes.push(analise);
+        } else if (analise.surveyField.field.city.region.macroRegion.name == "SUDOESTE" && sudoeste_bool == 1){
+            regioes.push(analise);
+        } else if (analise.surveyField.field.city.region.macroRegion.name == "OESTE" && oeste_bool == 1){
+            regioes.push(analise);
         } else {}
-
-        regioes[0] = norte;
-        regioes[1] = noroeste;
-        regioes[2] = sul;
-        regioes[3] = sudoeste;
-        regioes[4] = oeste;
 
     });
 
-    regioes = cleanArray(regioes); //Elimina arrays vazios dentro do array principal
+    regioes = cleanObject(regioes); //Elimina arrays vazios dentro do array principal
     //console.log(regioes);
     return regioes;
 
@@ -440,11 +424,11 @@ function filterByBt(bt_bool, dados){
 function prepare_ano(valor){
     return valor.split('/')[0];
 }
-//Limpa os arrays vazios dentro do array principal
-function cleanArray(actual) {
+//Limpa os objetos vazios dentro do objeto principal
+function cleanObject(actual) {
     var newArray = new Array();
     for (var i = 0; i < actual.length; i++) {
-      if (actual[i].length != 0) {
+      if (Object.keys(actual[i]).length !== 0) {
         newArray.push(actual[i]);
       }
     }
@@ -492,4 +476,18 @@ console.log(filterByAno(2018,
         filterByBt(1)))))))
         )));
 
+    
 console.log(filterByAno(2017, filterByRegion(1, 0, 0, 1, 0, ""))); // Não funciona, justamente porque o filterByRegion retorna um Array e não um Objeto. Corrigir isso.
+
+// console.log("Ano");
+// console.log(filterByAno(2017, ""));
+
+
+// console.log("REgião Norte");
+// console.log(filterByNorte());
+
+// console.log("REgião Norte e Sudoeste");
+// console.log(filterByRegion(1, 0, 0, 1, 0, ""));
+
+//console.log("REgião Norte e Sudoeste e 2017");
+//console.log(filterByAno(2017, filterByRegion(1, 0, 0, 1, 0, "")));
