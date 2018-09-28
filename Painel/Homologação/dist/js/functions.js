8cb65885755e69e5985135ec222ac20343c39eb1
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2018-09-27 23:39:22 -0300

    FilterByNorte

--- Painel/Homologação/dist/js/functions.js
+++ Painel/Homologação/dist/js/functions.js
@@ -6,7 +6,7 @@ Filtro por Ano
 */
 function filterByAno(ano, dados){
 
-    if(dados == ""){
+    if(dados == "" || dados == null){
         var data = JSON.parse(localStorage.getItem("dados"));
     } else {
         data = dados;
@@ -42,12 +42,73 @@ function prepare_ano(valor){
 }
 
 
+function filterByNorte(dados){
+    if(dados == "" || dados == null){
+        var data = JSON.parse(localStorage.getItem("dados"));
+    } else {
+        data = dados;
+    }
+
+    //console.log(data);
+    var i = 0;
+    var norte = [];
+
+    $.each(data, function(analises, analise) {
+        if((analise.surveyField.field.city.region.macroRegion.name).toUpperCase() === ("noRTe").toUpperCase()){
+            norte[i] = analise;
+            i++;
+        } 
+    });
+
+    if(i == 0){
+        alert("Região não encontrada");
+    }
+
+    //console.log(norte);
+    return norte;
+}
 
 function filterByRegion(){
+    var data = JSON.parse(localStorage.getItem("dados"));
+    console.log(data);
+    var norte = []; var n = 0;
+    var noroeste = []; var no = 0;
+    var sul = []; var s = 0;
+    var sudoeste = []; var se = 0;
+    var oeste = []; var oe = 0;
+    var regioes = [];
 
-    $.each(data, function(analises, analise){
+    $.each(data, function(i, item) {
+        //console.log(item.samplePestSet);
+
+        if(item.surveyField.field.city.region.macroRegion.name == "NORTE"){
+            norte[n] = item;
+            n++;
+        } else if (item.surveyField.field.city.region.macroRegion.name == "NOROESTE"){
+            noroeste[no] = item;
+            no++;
+        } else if (item.surveyField.field.city.region.macroRegion.name == "SUL"){
+            sul[s] = item;
+            s++;
+        } else if (item.surveyField.field.city.region.macroRegion.name == "SUDOESTE"){
+            sudoeste[se] = item;
+            se++;
+        } else if (item.surveyField.field.city.region.macroRegion.name == "OESTE"){
+            oeste[oe] = item;
+            oe++;
+        } else {}
+
+        regioes[0] = norte;
+        regioes[1] = noroeste;
+        regioes[2] = sul;
+        regioes[3] = sudoeste;
+        regioes[4] = oeste;
 
     });
+
+    console.log(regioes);
+    return regioes;
+
 }
 
 
@@ -60,3 +121,7 @@ function filterByRegion(){
 
 // Testes
 filterByAno(prepare_ano($( "#ano-da-safra" ).val()), ""); //Está funcionando
+
+//filterByRegion();
+
+filterByNorte();
