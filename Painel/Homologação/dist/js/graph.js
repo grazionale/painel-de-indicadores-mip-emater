e897af4ff91769320d2c1953499328dbdb6ca00c
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2018-10-10 00:51:54 -0300

    Alteração de Biblioteca de Gráficos - Concerto tabela de dados - correção layout responsivo

--- Painel/Homologação/dist/js/graph.js
+++ Painel/Homologação/dist/js/graph.js
@@ -156,9 +156,9 @@ function gerarTabelaDados(dados){
     $('#anticaria-0-30').html(valores[0].anticarsia.valor + "%");
     $('#anticaria-31-60').html(valores[1].anticarsia.valor + "%");
     $('#anticaria-61-150').html(valores[2].anticarsia.valor + "%");
-    $('#chrys-0-30').html(valores[0].spodoptera.valor + "%");
-    $('#chrys-31-60').html(valores[1].spodoptera.valor + "%");
-    $('#chrys-61-150').html(valores[2].spodoptera.valor + "%");
+    $('#chrys-0-30').html(valores[0].chrysodeixis.valor + "%");
+    $('#chrys-31-60').html(valores[1].chrysodeixis.valor + "%");
+    $('#chrys-61-150').html(valores[2].chrysodeixis.valor + "%");
     $('#spo-0-30').html(valores[0].spodoptera.valor + "%");
     $('#spo-31-60').html(valores[1].spodoptera.valor + "%");
     $('#spo-61-150').html(valores[2].spodoptera.valor + "%");
@@ -304,6 +304,340 @@ function gerarCanvasGraficoBarrasPercevejos(dados){
         }
     });
 }
+
+function HCgerarCanvasGraficoBarrasLagartas(dados){
+
+    var valores = generateDataForGraphBar(dados);
+
+    Highcharts.chart('canvas-generator-graph-bar', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['0-30', '31-60', '61-150'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'População (porcentagem)',
+                align: 'middle'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' %'
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.1,
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00'],
+        credits: {
+            enabled: false
+        },
+        exporting: { enabled: false },
+        series: [{
+            name: 'Anticarsia gemmatalis',
+            data: [parseFloat(valores[0].anticarsia.valor), parseFloat(valores[1].anticarsia.valor), parseFloat(valores[2].anticarsia.valor)]
+        }, {
+            name: 'Chrysodeixis includens',
+            data: [parseFloat(valores[0].chrysodeixis.valor), parseFloat(valores[1].chrysodeixis.valor), parseFloat(valores[2].chrysodeixis.valor)]
+        }, {
+            name: 'Spodoptera spp',
+            data: [parseFloat(valores[0].spodoptera.valor), parseFloat(valores[1].spodoptera.valor), parseFloat(valores[2].spodoptera.valor)]
+        }, {
+            name: 'Grupo Heliothinae',
+            data: [parseFloat(valores[0].heliothinae.valor), parseFloat(valores[1].heliothinae.valor), parseFloat(valores[2].heliothinae.valor)]
+        }]
+    });
+}
+
+function HCgerarCanvasGraficoBarrasPercevejos(dados){
+
+    var valores = generateDataForGraphBarPercevejos(dados);
+
+    Highcharts.chart('canvas-generator-graph-bar-percevejos', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['0-30', '31-60', '61-150'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'População (porcentagem)',
+                align: 'middle'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' %'
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.1,
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5'],
+        credits: {
+            enabled: false
+        },
+        exporting: { enabled: false },
+        series: [{
+            name: 'Nazera viridula',
+            data: [parseFloat(valores[0].nazera.valor), parseFloat(valores[1].nazera.valor), parseFloat(valores[2].nazera.valor)]
+        }, {
+            name: 'Piezodorus guildinii',
+            data: [parseFloat(valores[0].piezoduros.valor), parseFloat(valores[1].piezoduros.valor), parseFloat(valores[2].piezoduros.valor)]
+        }, {
+            name: 'Euschistus heros',
+            data: [parseFloat(valores[0].euschistus.valor), parseFloat(valores[1].euschistus.valor), parseFloat(valores[2].euschistus.valor)]
+        }, {
+            name: 'Dichelops melacanthus',
+            data: [parseFloat(valores[0].dichelops.valor), parseFloat(valores[1].dichelops.valor), parseFloat(valores[2].dichelops.valor)]
+        }, {
+            name: 'Outros',
+            data: [parseFloat(valores[0].outros.valor), parseFloat(valores[1].outros.valor), parseFloat(valores[2].outros.valor)]
+        }]
+    });
+}
+
+function HCgerarCanvasGraficoPizzaLagartas(dados){
+  
+    var valor_anticarsia = 0;
+    var valor_chrysodeixis = 0;
+    var valor_spodoptera = 0;
+    var valor_heliothinae = 0;
+
+    var num_anticarsia = 0;
+    var num_chrysodeixis = 0;
+    var num_spodoptera = 0;
+    var num_heliothinae = 0;
+
+    var media_anticarsia = 0;
+    var media_chrysodeixis = 0;
+    var media_spodoptera = 0;
+    var media_heliothinae = 0;
+
+    var lagartas = buscarLagartas(dados);
+    var cont = 0;
+
+    $.each(lagartas, function (lagarta, tipo) {
+        $.each(tipo, function (items, i) {
+            if (cont == 0) {
+                valor_anticarsia = valor_anticarsia + i.value;
+                num_anticarsia++;
+            } else if (cont == 1) {
+                valor_chrysodeixis = valor_chrysodeixis + i.value;
+                num_chrysodeixis++;
+            } else if (cont == 2) {
+                valor_spodoptera = valor_spodoptera + i.value;
+                num_spodoptera++;
+            } else {
+                valor_heliothinae = valor_heliothinae + i.value;
+                num_heliothinae++;
+            }
+        });
+        cont++;
+    });
+
+    media_anticarsia = (valor_anticarsia / num_anticarsia);
+    media_chrysodeixis = (valor_chrysodeixis / num_chrysodeixis);
+    media_spodoptera = (valor_spodoptera / num_spodoptera);
+    media_heliothinae = (valor_heliothinae / num_heliothinae);
+
+    var total_media = media_anticarsia + media_chrysodeixis + media_spodoptera + media_heliothinae;
+
+    var porcentagem_anticarsia = parseFloat((media_anticarsia * 100) / total_media).toFixed(2);
+    var porcentagem_chrysodeixis = parseFloat((media_chrysodeixis * 100) / total_media).toFixed(2);
+    var porcentagem_spodoptera = parseFloat((media_spodoptera * 100) / total_media).toFixed(2);
+    var porcentagem_heliothinae = parseFloat((media_heliothinae * 100) / total_media).toFixed(2);
+
+
+    var porcentagem_total = porcentagem_anticarsia + porcentagem_chrysodeixis + porcentagem_spodoptera + porcentagem_heliothinae;
+
+    Highcharts.chart('canvas-generator-graph-pizza', {
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+                }
+        },
+        colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00'],
+        credits: {
+            enabled: false
+        },
+        exporting: { enabled: false },
+        series: [{
+            name: 'Lagartas',
+            colorByPoint: true,
+            data: [{
+                name: 'Anticarsia gemmatalis',
+                y: parseFloat(porcentagem_anticarsia),
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Chrysodeixis includens',
+                y: parseFloat(porcentagem_chrysodeixis)
+            }, {
+                name: 'Spodoptera spp',
+                y: parseFloat(porcentagem_spodoptera)
+            }, {
+                name: 'Grupo Heliothinae',
+                y: parseFloat(porcentagem_heliothinae)
+            }]
+        }]
+    });
+}
+
+function HCgerarCanvasGraficoPizzaPercevejos(dados){
+  
+    var valor_nazera = 0;
+    var valor_piezodorus = 0;
+    var valor_euschistus = 0;
+    var valor_dichelops = 0;
+    var valor_outros = 0;
+
+    var num_nazera = 0;
+    var num_piezodorus = 0;
+    var num_euschistus = 0;
+    var num_dichelops = 0;
+    var num_outros = 0;
+
+    var percevejos = buscarPercevejos(dados);
+    var cont = 0;
+
+    $.each(percevejos, function (praga, tipo) {
+        $.each(tipo, function (items, i) {
+            if (cont == 0) {
+                valor_nazera = valor_nazera + i.value;
+                num_nazera++;
+            } else if (cont == 1) {
+                valor_piezodorus = valor_piezodorus + i.value;
+                num_piezodorus++;
+            } else if (cont == 2) {
+                valor_euschistus = valor_euschistus + i.value;
+                num_euschistus++;
+            } else if (cont == 3){
+                valor_dichelops = valor_dichelops + i.value;
+                num_dichelops++;
+            } else {
+                valor_outros = valor_outros + i.value;
+                num_outros++;
+            }
+        });
+        cont++;
+    });
+
+    var total_percevejos = valor_nazera + valor_piezodorus + valor_euschistus + valor_dichelops + valor_outros;
+
+    var percentagem_nazera = parseFloat((valor_nazera * 100) / total_percevejos).toFixed(2);
+    var porcentagem_piezodorus = parseFloat((valor_piezodorus * 100) / total_percevejos).toFixed(2);
+    var porcentagem_euschistus = parseFloat((valor_euschistus * 100) / total_percevejos).toFixed(2);
+    var porcentagem_dichelops = parseFloat((valor_dichelops * 100) / total_percevejos).toFixed(2);
+    var porcentagem_outros = parseFloat((valor_outros * 100) / total_percevejos).toFixed(2);
+
+    Highcharts.chart('canvas-generator-graph-pizza-percevejos', {
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+                }
+        },
+        colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5'],
+        credits: {
+            enabled: false
+        },
+        exporting: { enabled: false },
+        series: [{
+            name: 'Percevejos',
+            colorByPoint: true,
+            data: [{
+                name: 'Nazera viridula',
+                y: parseFloat(percentagem_nazera),
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Piezodorus guildinii',
+                y: parseFloat(porcentagem_piezodorus)
+            }, {
+                name: 'Euschistus heros',
+                y: parseFloat(porcentagem_euschistus)
+            }, {
+                name: 'Dichelops melacanthus',
+                y: parseFloat(porcentagem_dichelops)
+            }, {
+                name: 'Outros',
+                y: parseFloat(porcentagem_outros)
+            }]
+        }]
+    });
+}
+
 //Retorna dados filtrados a partir do filtro (front-end) que o usuário escolher.
 function filtrar(){
     var ano_safra = $("#ano-da-safra").val();
@@ -412,10 +746,15 @@ function filtrar(){
         return;
     }
     
-    gerarCanvasGraficoPizza(dados);
-    gerarCanvasGraficoBarras(dados);
+    //gerarCanvasGraficoPizza(dados);
+    //gerarCanvasGraficoBarras(dados);
     gerarTabelaDados(dados);
 
-    gerarCanvasGraficoPizzaPercevejos(dados);
-    gerarCanvasGraficoBarrasPercevejos(dados);
+    //gerarCanvasGraficoPizzaPercevejos(dados);
+    //gerarCanvasGraficoBarrasPercevejos(dados);
+
+    HCgerarCanvasGraficoBarrasLagartas(dados);
+    HCgerarCanvasGraficoBarrasPercevejos(dados);
+    HCgerarCanvasGraficoPizzaLagartas(dados);
+    HCgerarCanvasGraficoPizzaPercevejos(dados);
 }
