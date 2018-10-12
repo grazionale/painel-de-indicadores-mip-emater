fac044a3691d320e16f7a98fcdccd13d6dab94e8
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2018-10-12 01:06:13 -0300

    nada

--- Painel/Homologação/dist/js/loadJson.js
+++ Painel/Homologação/dist/js/loadJson.js
@@ -1,6 +1,6 @@
 function loadJson(){
-    //var api_url = "https://grazionale.github.io/dados.json";
-    var api_url = "https://siraa.com.br/dados4.json";
+    var api_url = "https://grazionale.github.io/dados.json";
+    //var api_url = "https://siraa.com.br/dados4.json";
     var dados = [];
 
     $.ajax({
@@ -12,7 +12,6 @@ function loadJson(){
         dataType: 'json',
         success: function (data) {
             localStorage.setItem("dados", JSON.stringify(data));
-            //console.log(localStorage.getItem("dados"));
         },
         error: function () {
             msg.text("Erro ao fazer requisição");
